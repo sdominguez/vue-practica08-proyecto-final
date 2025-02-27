1b78899e783978c24367fc5130b81d29e7d1d71a
Erick Abdiel Atzin Olarte <162238261+ERICKABDIEL@users.noreply.github.com>
2025-02-27 14:11:44 -0600

    commit en master1

---
+++ frontend/src/views/ErickView.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+    <hello-world />
+   </div>
+  </template>
+  
+  
+  <script>
+  import HelloWorld from '../components/HelloWorld'
+  
+  export default {
+    name: 'HomeView',
+  
+    data() {
+      return {
+        
+      };
+    },
+  
+    components: {
+      HelloWorld,
+    },
+    beforeCreate() {
+      if (!sessionStorage.getItem('usuario')) {
+        this.$router.push('/');
+      }
+    },
+    mounted(){
+      this.nombre_usuario = sessionStorage.getItem('usuario');
+      console.log(this.nombre_usuario);
+    },
+    
+  }
+  </script>
+  
